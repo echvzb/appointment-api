e70c6cd8d8d46ba905baf94ba502547d5afba1c5
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-03-08 00:41:59 -0600

    feat: send time events in a certain date week

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,13 +1,84 @@
 const calendarRouter = require("express").Router();
 const { google } = require("googleapis");
 const { getOAuth2Client } = require("../utils/authClient");
+const formatRFC3339 = require("date-fns/formatRFC3339");
+const sub = require("date-fns/sub");
+const add = require("date-fns/add");
 
 calendarRouter.get("/", async (req, res) => {
-  const oauth20Client = getOAuth2Client(req.user.accessToken);
+  const { tokens } = req.user;
+  const { date } = req.query;
+  const parsedDate = new Date(date);
+  parsedDate.setHours(0, 0, 0, 0);
+  const today = parsedDate.getDay();
+
+  const firstDayOfTheWeek = sub(parsedDate, { days: today });
+  const lastDayOfTheWeek = add(firstDayOfTheWeek, { days: 6 });
+  lastDayOfTheWeek.setHours(23, 59, 59, 999);
+
+  const oauth20Client = getOAuth2Client(tokens);
   const calendar = google.calendar({ version: "v3", auth: oauth20Client });
+
   try {
-    const calendarList = await calendar.calendarList.list();
-    res.json(calendarList.data.items);
+    const ownersEvents = await calendar.events.list({
+      calendarId: "primary",
+      timeMin: formatRFC3339(firstDayOfTheWeek),
+      timeMax: formatRFC3339(lastDayOfTheWeek),
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+
+    const { data } = ownersEvents;
+    const items = Array(...data.items);
+    const events = [[], [], [], [], [], [], []];
+
+    for (const item of items) {
+      if (item.start.dateTime) {
+        const start = new Date(item.start.dateTime);
+        const end = new Date(item.end.dateTime);
+        const day = start.getDay();
+        const currentDayEventsLength = events[day].length;
+        const prevEvent =
+          currentDayEventsLength > 0
+            ? events[day][currentDayEventsLength - 1]
+            : null;
+
+        if (prevEvent) {
+          const startTime = start.getTime();
+          const endTime = end.getTime();
+
+          const [prevStartDate, prevEndDate] = prevEvent
+            .split("-")
+            .map((time, i) => {
+              const prevTimeDate = new Date(i === 0 ? startTime : endTime);
+              prevTimeDate.setHours(...time.split(":").map(Number));
+              return prevTimeDate;
+            });
+
+          const prevStartTime = prevStartDate.getTime();
+          const prevEndTime = prevEndDate.getTime();
+
+          if (startTime >= prevStartTime && startTime <= prevEndTime) {
+            if (endTime >= prevEndTime) {
+              events[day][currentDayEventsLength - 1] = `${
+                prevEvent.split("-")[0]
+              }-${end.getHours()}:${end.getMinutes()}`;
+            }
+            continue;
+          }
+        }
+        const event =
+          start.getHours() +
+          ":" +
+          start.getMinutes() +
+          "-" +
+          end.getHours() +
+          ":" +
+          end.getMinutes();
+        events[day].push(event);
+      }
+    }
+    res.json(events);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- utils/authClient.js
+++ utils/authClient.js
@@ -1,12 +1,13 @@
 const { google } = require("googleapis");
 
-const getOAuth2Client = (access_token) => {
+const getOAuth2Client = (tokens) => {
   const oauth20Client = new google.auth.OAuth2({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   });
   oauth20Client.setCredentials({
-    access_token,
+    access_token: tokens.accessToken,
+    refresh_token: tokens.refreshToken,
   });
   return oauth20Client;
 };
