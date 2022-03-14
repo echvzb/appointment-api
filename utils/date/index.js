97587c3cc0465f6b4af142ecdbb25784c766a94f
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-03-13 21:11:33 -0600

    refactor: remove tzOffset

--- utils/date/index.js
+++ utils/date/index.js
@@ -1,11 +1,10 @@
 const parseGoogleDate = (d) => {
   const googleDate =
     /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})([+-]\d{2}):(\d{2})/g;
-  const [, year, month, day, hours, minutes, seconds, tzHour, tzMinute] =
+  const [, year, month, day, hours, minutes, seconds] =
     googleDate.exec(d);
-  const tzOffset = +tzHour * 60 + +tzMinute;
   return new Date(
-    Date.UTC(+year, +month - 1, +day, +hours, +minutes - tzOffset, +seconds)
+    Date.UTC(+year, +month - 1, +day, +hours, +minutes, +seconds)
   );
 };
 
