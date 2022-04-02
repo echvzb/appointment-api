1df63f14b64b0f756f64a481a0b1e20c742ee56d
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-04-01 23:37:22 -0600

    feat: add getUTCDate func

--- utils/date/index.js
+++ utils/date/index.js
@@ -1,11 +1,22 @@
 const parseGoogleDate = (d) => {
   const googleDate =
     /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})([+-]\d{2}):(\d{2})/g;
-  const [, year, month, day, hours, minutes, seconds] =
-    googleDate.exec(d);
+  const [, year, month, day, hours, minutes, seconds] = googleDate.exec(d);
   return new Date(
     Date.UTC(+year, +month - 1, +day, +hours, +minutes, +seconds)
   );
 };
 
-module.exports = { parseGoogleDate };
+const getDateValuesFromDateString = (dateStr) => {
+  const [date, time] = dateStr.split("T");
+  const [year, month, day] = date.split("-");
+  const [hours, minutes] = time.split(":");
+
+  return [+year, +month - 1, +day, +hours, +minutes];
+};
+
+const getDateFromDateString = (dateStr) => {
+  return new Date(Date.UTC(...getDateValuesFromDateString(dateStr)));
+};
+
+module.exports = { parseGoogleDate, getDateFromDateString };
