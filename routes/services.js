170e0eb9bd569ab19980ff7a84881a56c5b9d04b
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-04-23 22:41:26 -0500

    feat: add get services endpoint

--- routes/services.js
+++ routes/services.js
@@ -3,19 +3,39 @@ const {
   models: { Service },
 } = require("../db");
 
+router.use((req, res, next) => {
+  if (req.user.config.isBusinessAccount) {
+    next();
+  } else {
+    res.status(403).json({
+      error: "The account is not a business account",
+    });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
-    if (req.user.config.isBusinessAccount) {
-      const { name, timeInMinutes } = req.body;
-      await Service.create({
-        name,
-        timeInMinutes,
+    const { name, timeInMinutes } = req.body;
+    await Service.create({
+      name,
+      timeInMinutes,
+      businessId: req.user._id,
+    });
+    res.status(201).end();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const services = await Service.find(
+      {
         businessId: req.user._id,
-      });
-      res.status(201).end();
-    } else {
-      res.status(403).json({ error: "the account is not a business account" });
-    }
+      },
+      "name timeInMinutes"
+    );
+    res.status(200).json(services);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
